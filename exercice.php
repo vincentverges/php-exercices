0e1fb00ba470336ef5d851ddfd6598537f565805
Cheick  Diabate <cheick.diabate@gmail.com>
2024-06-24 14:16:32 +0200

    exohamad

---
+++ exercice.php
@@ -0,0 +1,9 @@
+hello world
+<?php  
+$nom = "Diabate";
+echo "$nom";
+?>
+<?php
+$commentaire = "Une variable est un conteneur qui peut stocker des données et dont la valeur peut changer au cours de l'exécution d'un script.";
+echo "$commentaire";
+?>
