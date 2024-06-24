3dd293059de8a21ac2f7ba9916d947dd97de3de4
Vincent Verges <me@vincentverges.com>
2024-06-24 14:37:21 +0200

    Correction ex1

--- exercice1.php
+++ exercice1.php
@@ -1,3 +1,19 @@
 <?php
-'rdjhn'
+// commentaire en une ligne
+# la aussi en une ligne
+/*
+Commentaire sur plusieurs lignes
+*/
+
+echo "Hello world";
+
+$nom = "Bambi";
+
+echo $nom;
+
+/*
+Une variable est une valeur qui peut etre modifier (ecriture en camel ou snake case)
+Une constante est une valeur immuable, elle ne peut donc pas etre modifier (Ecris en capital)
+*/
+
 ?>
