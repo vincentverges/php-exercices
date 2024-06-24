2ca8f9cd0f6d727cd3dcf97e940c28561208efca
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-06-24 14:05:54 +0200

    add comment

--- exercice1.php
+++ exercice1.php
@@ -1,5 +1,10 @@
 <?php
+/*
+Une variable contient une valeur que l'on va modifier
+Une constante contient une valeur qui va rester stable et ne sera pas modifiée
+*/
 $nom= "Claire"; //Definition de la varialbe string
 echo "Hello world";
 echo "Je m'appelle {$nom}."; #Affichage de la variable dans une phrase
 ?>
+
