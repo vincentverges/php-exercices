ee394b167716db804088421d82738e547cee0fc4
Moumen Bourahla <moumen.bourahla@le-campus-numerique.fr>
2024-06-24 14:07:00 +0200

    Up

---
+++ exercice1.php
@@ -0,0 +1,7 @@
+<?php
+//Première manière de mettre un commentaire "//" : pour mettre un commentaire sur une seule ligne style c++
+/*Deuxième manière de mettre un commentaire "/*" :
+pour en faire sur plusieurs lignes et fermer avec : */
+# Troisième manière de mettre un commentaire "#" : commentaire sur une seule ligne style shell
+echo "Hello Worl"
+?>
