640620dba12f8fcc427b94e65e4e08aae7b9ab04
Adonat74 <donatgoninet.antoine@gmail.com>
2024-06-24 14:02:29 +0200

    exo 1

---
+++ exercice1.php
@@ -0,0 +1,14 @@
+<?php
+// slash 
+# hashtag 
+/*multi line */
+
+echo "Hello World!";
+
+$nom = "Dom Toretto";
+
+echo " My name is $nom and family is the most important thing."
+
+// Une constante est une variable dont la valeur une fois initialisée ne peut plus changer contrairement à une variable.
+
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+
+
+<?php
+include "exercice1.php";
+?>
