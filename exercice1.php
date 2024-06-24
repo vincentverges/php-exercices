2d6492c86faf53ff014e1c1b8ccf5c6e885e0ea8
Eric Oriez <ericoriez@live.fr>
2024-06-24 14:04:02 +0200

    exercice1

---
+++ exercice1.php
@@ -0,0 +1,12 @@
+<?
+
+echo "hello world";
+
+$nom = 'Eric';
+
+echo "bonjour, $nom";
+
+// Variable : Stocke une valeur pouvant être modifiée pendant l'exécution du programme.
+
+// Constante : Stocke une valeur qui ne peut pas être modifiée une fois définie.
+?>
