e808c3ccab582d086277c498e0ff08476291d2af
RDelory74 <roro_spike@hotmail.com>
2024-06-24 14:41:11 +0200

    up exercice1

--- exercice1.php
+++ exercice1.php
@@ -3,7 +3,7 @@
 <?php
     $nom = 'Hello World';
     echo $nom;
-    echo 'Hello World';
+    echo 'Hello World'.$nom;
 ?>
 
 // Une constante est un identifiant immuable, alors que la variable est ammené à changer de valeur en fonction de l'évolution du programme
