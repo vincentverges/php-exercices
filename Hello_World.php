65e99e15dfde1088af0be955b4b62eec69f3357b
poetta <pedro.costa@le-campus-numerique.fr>
2024-06-24 14:02:41 +0200

    my first php file

---
+++ Hello_World.php
@@ -0,0 +1,21 @@
+<?php
+// Ceci est un commentaire sur une seule ligne
+
+/*
+Ceci est un commentaire
+sur plusieurs lignes
+*/
+
+# Ceci est aussi un commentaire sur une seule ligne
+
+// Affichage du texte "Hello World"
+echo "Hello World\n";
+
+// Création d'une variable $nom avec une valeur String
+$nom = "Poetta";
+
+// Intégration de la variable $nom dans un texte et affichage
+echo "Bonjour, mon nom est $nom.\n";
+
+// Affichage de la différence entre variable et constante
+echo "$nom dit qu'une variable est un conteneur qui peut stocker des valeurs qui peuvent changer au cours de l'exécution du script. Cependant, une constante est un conteneur qui stocke une valeur fixe, définie une seule fois et ne peut pas être modifiée pendant l'exécution du script.\n";
