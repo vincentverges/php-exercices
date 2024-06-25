b17b2181bd4a835a78d00dc914d7c71d7b74e8b5
Cheick  Diabate <cheick.diabate@gmail.com>
2024-06-25 14:05:54 +0200

    exo2

--- exercice1.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "Hello World";
-$nom = "chadeuf";
-echo"je m'appel tristan $nom"
-?>
---
+++ exercice2.php
@@ -0,0 +1,41 @@
+<?php
+$age =10; 
+if ($age >= 18) {
+    echo "Vous êtes majeur.";
+} elseif ($age < 18) {
+    echo "Vous êtes mineur.";
+}
+?>
+
+
+<?php
+$heure = 13;
+if ($heure < 12){
+        echo "Bonjour";
+}elseif ($heure < 18) {
+      echo "Bon après-midi";
+    }else{
+      echo "bonsoir";
+   }
+?>
+
+<?php
+$utilisteur = "bonjourrrr";
+$motdePasse = "bonsoirrrr";
+if ($utilisateur === "admin" && $motDePasse === "1234")
+echo "Accès autorisé."
+} else {
+    echo "Accès refusé."; 
+}
+?>
+
+<?php
+
+$utilisateur = "admin";
+$motDePasse = "1234";
+if ($utilisateur === "admin" && $motDePasse === "1234") {
+    echo "Connexion réussie!"; 
+} else {
+    echo "Nom d'utilisateur ou mot de passe incorrect."; 
+}
+?>
