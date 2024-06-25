a1eae1cec6e757befd6972ed569de019a72af4ef
RDelory74 <roro_spike@hotmail.com>
2024-06-25 14:26:45 +0200

    up exo

--- exercice2.php
+++ exercice2.php
@@ -5,19 +5,23 @@ $heure = date("H:i:s");
 
 echo "Il est $heure";
 echo '<br/>';
-if ($age > 18) {
+if ($age >= 18) {
     echo "Et tu es majeur !!!!";
-}
+} else echo " vous êtes mineur";
+
 echo '<br/>';
+
 if ($heure < date("12:00:00")) {
     echo "Bonjour";
 } elseif ($heure < date("18:00:00")) {
     echo "Bon-après midi";
 } else echo "Bonsoir";
+
 echo '<br/>';
+
 $utilisateur = 'admin';
 $motDePass = '1234';
 
 if ($utilisateur == 'admin' & $motDePass == '1234') {
     echo "Connection reussie !!!";
-} else echo "Mot de passe incorrect";
+} else echo "Nom d'utilisateur ou Mot de passe incorrect";
