f354567252ffd9e845901b85c164aa1483a88324
Tristan Chadeuf <tristan.chadeuf@le-campus-numerique.fr>
2024-06-25 13:59:42 +0200

    exercice2

---
+++ exercice2.php
@@ -0,0 +1,40 @@
+<?php
+
+function age($age){
+if($age>=18){
+echo"Vous etes Majeur";
+}
+else{
+    echo"Vous etes mineur";
+
+}
+}
+
+function Heure($heure){
+if($heure<12){
+echo"bonjour";
+}
+elseif($heure<18){
+echo"Bon après-midi";
+}
+else{
+    echo"Bonsoir";
+}
+}
+
+
+function Connection(){
+$utilisateur= "admin";
+$motDePasse="1234";
+
+if($utilisateur='admin'){
+
+}
+elseif($motDePasse='1234'){
+
+}
+
+}
+
+
+?>
