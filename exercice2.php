1663b4b2ea96cb301669e017c7cb4adc2dc4b596
RDelory74 <roro_spike@hotmail.com>
2024-06-25 13:48:55 +0200

    featex2 and done

---
+++ exercice2.php
@@ -0,0 +1,23 @@
+<?php
+
+$age = 19;
+$heure = date("H:i:s");
+
+echo "Il est $heure";
+echo '<br/>';
+if ($age > 18) {
+    echo "Et tu es majeur !!!!";
+}
+echo '<br/>';
+if ($heure < date("12:00:00")) {
+    echo "Bonjour";
+} elseif ($heure < date("18:00:00")) {
+    echo "Bon-après midi";
+} else echo "Bonsoir";
+echo '<br/>';
+$utilisateur = 'admin';
+$motDePass = '1234';
+
+if ($utilisateur == 'admin' & $motDePass == '1234') {
+    echo "Connection reussie !!!";
+} else echo "Mot de passe incorrect";
