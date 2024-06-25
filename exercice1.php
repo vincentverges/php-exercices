935670d84c43eb452a8bb56b45fddcd5eddddeab
Eric Oriez <ericoriez@live.fr>
2024-06-25 13:36:45 +0200

    exo2

--- exercice1.php
+++ exercice1.php
@@ -9,4 +9,5 @@ echo "bonjour, $nom";
 // Variable : Stocke une valeur pouvant être modifiée pendant l'exécution du programme.
 
 // Constante : Stocke une valeur qui ne peut pas être modifiée une fois définie.
+
 ?>
