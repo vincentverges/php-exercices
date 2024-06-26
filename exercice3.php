58db18717736fd9d4ceff398960814e5f99c4322
Moumen Bourahla <moumen.bourahla@le-campus-numerique.fr>
2024-06-26 14:07:16 +0200

    exo3

---
+++ exercice3.php
@@ -0,0 +1,31 @@
+<?php 
+
+$fruits=array(
+    "Pomme", "Banane", "Orange", "Mangue"
+);
+
+echo $fruits[1];
+
+foreach ($fruits as $case) {
+    $case = $case++;
+    echo $case;
+}
+
+$personne=array(
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
