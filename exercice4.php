0d04463cf0765011fd0069ae6a0dde8e2fb6158f
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-06-27 13:46:11 +0200

    add file ex4 with last function not working

---
+++ exercice4.php
@@ -0,0 +1,24 @@
+<?php
+function direBonjour() {
+   echo ("Bonjour à tous !");
+}
+;
+$nom = "Claire";
+ function direBonjourA($nom) {
+    echo ("Bonjour" .$nom. ".");
+};
+$a = 2;
+$b = 5;
+function addition($a, $b) {
+    $somme = $a + $b;
+    echo($somme);
+};
+$x = 4;
+$y = 5;
+function multiplier ($x, $y){
+    $multiple= $x * $y;
+    return $multiple;
+};
+echo ($multiple);
+?>
+
