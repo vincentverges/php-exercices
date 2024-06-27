5a2114f424ec0675e7f99be98102f9280df3fd0a
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-06-27 13:50:39 +0200

    added missing lines for function calls

--- exercice4.php
+++ exercice4.php
@@ -3,16 +3,19 @@ function direBonjour() {
    echo ("Bonjour à tous !");
 }
 ;
+direBonjour();
 $nom = "Claire";
  function direBonjourA($nom) {
     echo ("Bonjour" .$nom. ".");
 };
+direBonjourA($nom);
 $a = 2;
 $b = 5;
 function addition($a, $b) {
     $somme = $a + $b;
     echo($somme);
 };
+addition($a, $b);
 $x = 4;
 $y = 5;
 function multiplier ($x, $y){
