4338c3958bbdccb5c74bb4002282b7a5ea5c20f3
Eric Oriez <ericoriez@live.fr>
2024-06-27 14:01:41 +0200

    updateExo4

--- exercice4.php
+++ exercice4.php
@@ -1,30 +1,48 @@
 <?php 
 
+// Créer une fonction sans paramètres :
 function direBonjour(){
 
 echo 'Bonjour a tous !';
 
-
 }
+// Appel de la function
 direBonjour();
+
+
+
+
+// Créer une fonction avec un paramètre :
 function direBonjourA($nom){
 
     echo "Bonjour,$nom";
 }
+// Appel de la function avec paramètre
 direBonjourA("pierre");
 
+
+
+
+// Créer une fonction avec plusieurs paramètres :
 function addition($a, $b){
 
     echo $a + $b;
 }
-
+// Appel de la function avec 2 paramètre
 addition(7,7);
+
+
+
+
+
+// Créer une fonction avec une valeur de retour :
 function multiplier($x, $y){
     
     $sum = $x * $y;
     echo $sum;
     return $sum;
 }
+// Appel de la function avec 2 paramètre, récupération du résultat stocker dans une variable et renvoyer avec le return.
 multiplier(5,5);
 
 
